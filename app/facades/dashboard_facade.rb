ad08d1e9c767ced73541c1c9611bf07bc066c5cb
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-03-20 15:35:41 -0600

    Renamed DashboardRepoFacade to DashboardFacade and added methods for followers
    Co-authored-by: Scott Thomas <31359242+smthom05@users.noreply.github.com>

---
+++ app/facades/dashboard_facade.rb
@@ -0,0 +1,25 @@
+class DashboardFacade
+
+  def initialize(user_token)
+    @user_token = user_token
+  end
+
+  def repos
+    response = service.get_repos
+    response.map do |repo_data|
+      Repo.new(repo_data)
+    end[0..4]
+  end
+
+  def followers
+    response = service.get_followers
+    response.map do |follower_data|
+      Follower.new(follower_data)
+    end
+  end
+
+  def service
+    GithubService.new(@user_token)
+  end
+
+end
