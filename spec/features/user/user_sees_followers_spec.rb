0b3da62cdf451914bb2c03c7eb6cfdffb262c33d
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-03-20 15:22:11 -0600

    Feature test for user's followers

---
+++ spec/features/user/user_sees_followers_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'As a logged in user' do
+  context 'when visiting /dashboard' do
+    it 'sees all followers' do
+      VCR.use_cassette('github_current_users_followers', :allow_playback_repeats => true) do
+        VCR.use_cassette('github_current_users_repos', :allow_playback_repeats => true) do
+          user = create(:user, token: ENV['github_user_token'])
+          login_as(user)
+
+          expect(page).to have_content('Followers')
+          expect(page).to have_css('.followers')
+
+          facade = DashboardFacade.new(ENV['github_user_token'])
+
+          counter = 0
+          facade.followers.each do |follower|
+            expect(page).to have_css('.follower')
+
+            within(page.all('.follower')[counter]) do
+              expect(page).to have_css(".#{follower.handle}")
+              expect(page).to have_link(follower.handle, href: follower.url)
+            end
+            counter += 1
+          end
+        end
+      end
+    end
+  end
+end
