1a4fd6792edb8fdc71ede031eeab3e0f3b972de0
Scott Thomas <smthom05@gmail.com>
2019-03-21 08:08:06 -0600

    Created Followed class, added counter increasing to the user can see followed users feature tests
    Co-authored-by: Peregrine Balas <39976770+PeregrineReed@users.noreply.github.com>

---
+++ app/models/followed.rb
@@ -0,0 +1,10 @@
+class Followed
+  attr_reader :handle,
+              :url
+              
+  def initialize(data)
+    @handle = data[:login]
+    @url = data[:html_url]
+  end
+
+end
--- spec/features/user/user_can_see_followed_users_spec.rb
+++ spec/features/user/user_can_see_followed_users_spec.rb
@@ -15,14 +15,15 @@ feature "As a logged in user" do
             facade = DashboardFacade.new(ENV['github_user_token'])
 
             counter = 0
-
+            save_and_open_page
             facade.followed.each do |f|
               expect(page).to have_css('.followed-user')
 
-              within (page.all('followed-user')[counter]) do
+              within (page.all('.followed-user')[counter]) do
                 expect(page).to have_css(".#{f.handle}")
                 expect(page).to have_link(f.handle, href: f.url)
               end
+              counter += 1
             end
           end
         end
