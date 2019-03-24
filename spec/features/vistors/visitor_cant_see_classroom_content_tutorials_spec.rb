d826e96cd1b75636ffd68743a22bf2e13ab7d29f
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-03-24 15:40:01 -0600

    Added to visitor homepage spec to expect visitors to only see non classroom tutorials
    Co-authored-by: Scott Thomas <31359242+smthom05@users.noreply.github.com>

--- spec/features/vistors/visitor_cant_see_classroom_content_tutorials_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-feature 'As a visitor' do
-  context 'when visiting /' do
-    it 'only sees non-classroom content tutorials' do
-    end
-  end
-end
--- spec/features/vistors/visitor_sees_homepage_spec.rb
+++ spec/features/vistors/visitor_sees_homepage_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe 'Visitor' do
   describe 'on the home page' do
-    it 'can see a list of tutorials' do
+    it 'can see a list of non-classroom tutorials' do
       tutorial1 = create(:tutorial)
-      tutorial2 = create(:tutorial)
+      tutorial2 = create(:tutorial, classroom: true)
 
       video1 = create(:video, tutorial_id: tutorial1.id)
       video2 = create(:video, tutorial_id: tutorial1.id)
@@ -13,7 +13,7 @@ describe 'Visitor' do
 
       visit root_path
 
-      expect(page).to have_css('.tutorial', count: 2)
+      expect(page).to have_css('.tutorial', count: 1)
 
       within(first('.tutorials')) do
         expect(page).to have_css('.tutorial')
@@ -21,6 +21,9 @@ describe 'Visitor' do
         expect(page).to have_content(tutorial1.title)
         expect(page).to have_content(tutorial1.description)
       end
+
+      expect(page).to_not have_content(tutorial2.title)
+      expect(page).to_not have_content(tutorial2.description)
     end
   end
 end
