49671ccc052f05ccfb624c605650a8b7237e4ed4
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-03-24 15:42:15 -0600

    Removed commented out code
    Co-authored-by: Scott Thomas <31359242+smthom05@users.noreply.github.com>

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    # if params[:tag]
-      render locals: {
-        facade: WelcomeFacade.new(params, current_user)
-      }
-    #   @tutorials = Tutorial.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 5)
-    # else
-    #   @tutorials = Tutorial.all.paginate(page: params[:page], per_page: 5)
-    # end
+    render locals: {
+      facade: WelcomeFacade.new(params, current_user)
+    }
   end
 end
