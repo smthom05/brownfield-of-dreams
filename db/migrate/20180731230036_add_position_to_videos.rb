ebc44e9bd0eff090472fbfd11ad4ccf7ce1c12fd
Peregrine Reed Balas <39976770+PeregrineReed@users.noreply.github.com>
2019-03-25 13:14:10 -0600

    Revert "Revert "Video""

--- db/migrate/20180731230036_add_position_to_videos.rb
+++ db/migrate/20180731230036_add_position_to_videos.rb
@@ -1,4 +1,6 @@
+# migration to add positon column to Video class
 class AddPositionToVideos < ActiveRecord::Migration[5.2]
+  # frozen_string_literal: true
   def change
     add_column :videos, :position, :integer, default: 0
   end
--- db/migrate/20190323211727_create_friends.rb
+++ db/migrate/20190323211727_create_friends.rb
@@ -1,4 +1,6 @@
+# migration to create Friends self refential table for User class
 class CreateFriends < ActiveRecord::Migration[5.2]
+  # frozen_string_literal: true
   def change
     create_table :friends do |t|
       t.references :user, foreign_key: { to_table: :users }
