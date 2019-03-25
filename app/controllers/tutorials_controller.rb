8604ea926b9c8053f557d1a1cccb3e68b97d91ae
Peregrine Reed Balas <39976770+PeregrineReed@users.noreply.github.com>
2019-03-25 15:29:36 -0600

    Pry
    removed commented out binding.pry

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -2,6 +2,5 @@ class TutorialsController < ApplicationController
   def show
     tutorial = Tutorial.find(params[:id])
     @facade = TutorialFacade.new(tutorial, params[:video_id])
-    # binding.pry
   end
 end
