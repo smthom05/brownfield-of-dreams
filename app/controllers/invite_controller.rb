f3c668cd9464e4e43021be567f69848e4c017681
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-03-27 18:39:25 -0600

    Created invite controller
    Co-authored-by: Scott Thomas <31359242+smthom05@users.noreply.github.com>

---
+++ app/controllers/invite_controller.rb
@@ -0,0 +1,19 @@
+class InviteController < ApplicationController
+
+  def new
+  end
+
+  def create
+    facade = InviteFacade.new(current_user.token)
+    inviter = facade.inviter
+    invitee = facade.email(params[:github_handle])
+    unless invitee[:name] == nil
+      InviteMailer.invite(inviter, invitee).deliver_now
+      flash[:success] = 'Successfully sent invite!'
+    else
+      flash[:error] = 'The Github user you selected doesn\'t have an email address associated with their account.'
+    end
+    redirect_to dashboard_path
+  end
+
+end
