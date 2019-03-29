684921587edd0731dd5d6140b9ecfffdb9b11550
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-03-28 22:05:57 -0600

    Cleared rubocop offenses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create # rubocop:disable Metrics/MethodLength
+  def create
     user = User.create(user_params)
     if user.save
       session[:user_id] = user.id
